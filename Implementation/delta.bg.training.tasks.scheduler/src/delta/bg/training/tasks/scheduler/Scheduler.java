dc5adc169befffdd7c6c215ca3989023001c1407
MiroslavRusenov <rusenov.miroslav@gmail.com>
2014-04-02 09:16:32 +0300

    Update Scheduler.java

--- Implementation/delta.bg.training.tasks.scheduler/src/delta/bg/training/tasks/scheduler/Scheduler.java
+++ Implementation/delta.bg.training.tasks.scheduler/src/delta/bg/training/tasks/scheduler/Scheduler.java
@@ -7,10 +7,146 @@ import delta.bg.training.tasks.scheduler.Employee;
 public class Scheduler {
 	// TODO: Task 3
 	// declare private static variables: workdays , numWorkplaces , etc.
-
+	private static boolean[] workdays;
+	private static int numWorkplaces = 0;
+	private static int numEmployees = 0;
+	private static int workdayStart = 0;
+	private static int workdayEnd = 0;
+	private static int numShifts = 0;
+	private static int hoursInShift = 0;
+	private static int breakBetweenShifts = 0;
+	private static int[][][] occupiedWorkplace;
+	private static LinkedList<Employee> employees;
+	private static int minWorkHours = 0;
+	private static int maxWorkHours = 0;
+	
 	// create get-ers and set-ers for every private variable.
 	// Add necessary verifications for correct values on the set-ers
 	// Use the variables ONLY with the get-ers and set-ers
+	public static boolean[] getWorkdays() {
+		return workdays;
+	}
+
+	// Check that workdays.length() == 7
+	public static void setWorkdays(boolean[] workdays) {
+		if (workdays.length == 7) {
+			Scheduler.workdays = workdays;
+		}
+	}
+
+	public static int getNumWorkplaces() {
+		return numWorkplaces;
+	}
+
+	public static void setNumWorkplaces(int numWorkplaces) {
+		if (numWorkplaces >= 1) {
+			Scheduler.numWorkplaces = numWorkplaces;
+		}
+	}
+
+	public static int getNumEmployees() {
+		return numEmployees;
+	}
+
+	public static void setNumEmployees(int numEmployees) {
+		if (numEmployees >= 1) {
+			Scheduler.numEmployees = numEmployees;
+		}
+	}
+
+	public static int getWorkdayStart() {
+		return workdayStart;
+	}
+
+	// check that workdayStart > 0 && workdayStart < 24
+	public static void setWorkdayStart(int workdayStart) {
+		if (workdayStart > 0 && workdayStart < 24) {
+			Scheduler.workdayStart = workdayStart;
+		}
+	}
+
+	public static int getWorkdayEnd() {
+		return workdayEnd;
+	}
+
+	// check that workdayEnd > 0 && workdayEnd < 24
+	public static void setWorkdayEnd(int workdayEnd) {
+		if (workdayEnd > 0 && workdayEnd < 24) {
+			Scheduler.workdayEnd = workdayEnd;
+		}
+	}
+
+	public static int getNumShifts() {
+		return numShifts;
+	}
+
+	public static void setNumShifts(int numShifts) {
+		if (numShifts >= 1) {
+			Scheduler.numShifts = numShifts;
+		}
+	}
+
+	public static int getHoursInShift() {
+		return hoursInShift;
+	}
+
+	// also hoursInShift < 24 (or 8, but let's say 24)
+	public static void setHoursInShift(int hoursInShift) {
+		if (hoursInShift >= 1 && hoursInShift < 24) {
+			Scheduler.hoursInShift = hoursInShift;
+		}
+	}
+
+	public static int getBreakBetweenShifts() {
+		return breakBetweenShifts;
+	}
+
+	// should be > 0 and < 24
+	public static void setBreakBetweenShifts(int breakBetweenShifts) {
+		if (breakBetweenShifts > 0 && breakBetweenShifts < 24) {
+			Scheduler.breakBetweenShifts = breakBetweenShifts;
+		}
+	}
+
+	public static int[][][] getOccupiedWorkplace() {
+		return occupiedWorkplace;
+	}
+
+	public static void setOccupiedWorkplace(int[][][] occupiedWorkplace) {
+		Scheduler.occupiedWorkplace = occupiedWorkplace;
+	}
+
+	public static LinkedList<Employee> getEmployees() {
+		return employees;
+	}
+
+	public static void setEmployees(LinkedList<Employee> employees) {
+		Scheduler.employees = employees;
+	}
+
+	public static int getMinWorkHours() {
+		return minWorkHours;
+	}
+
+	// check minWorkHours <= maxWorkHours only if maxWorkHours > 0
+	public static void setMinWorkHours(int minWorkHours) {
+		if (maxWorkHours > 0) {
+			if (minWorkHours <= maxWorkHours) {
+				Scheduler.minWorkHours = minWorkHours;
+			}
+		}
+	}
+
+	public static int getMaxWorkHours() {
+		return maxWorkHours;
+	}
+
+	// maxWorkHours > minWorkHours (not >= 1)
+	public static void setMaxWorkHours(int maxWorkHours) {
+		if (maxWorkHours > minWorkHours && maxWorkHours <= 80) {
+			Scheduler.maxWorkHours = maxWorkHours;
+		}
+	}
 	
 	// filename is the name of the Employee.in file. Task 2 
 	// has the read functionality
